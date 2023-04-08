0fe6812236ee1fc696546174df2874320bac2eec
Chu Van Tuyen <chuvantuyen512@gmail.com>
2023-04-08 22:15:50 +0700

    xong bt4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Bt1Component } from './bt1/bt1.component';
+import { HelloComponent } from './hello/hello.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'bai-tap',
+    component: Bt1Component,
+  },
+  {
+    path: '',
+    component: HelloComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
